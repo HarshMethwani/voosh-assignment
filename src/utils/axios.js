9a3ced63f7f9a370432bedc3954d6ef460a55efa
HarshMethwani <hmethwani21@iitk.ac.in>
2024-11-30 03:41:13 +0530

    axios error

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://ec2-3-110-195-114.ap-south-1.compute.amazonaws.com:5000/api", // Update to your backend's base URL
+  baseURL: "http://ec2-3-110-195-114.ap-south-1.compute.amazonaws.com:5000/api", 
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,// Update to your backend's base URL
 });
 
 // Add request interceptor to include Authorization header
